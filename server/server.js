1cc37edebb2d8ca950c2fe2869f479dbb7d8cc56
Mohit Gupta <mohitguptaa0110@gmail.com>
2025-07-20 17:41:13 +0530

    clerk check

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(clerkMiddleware());
 
 // API to listen to Clerk Webhooks
-app.use("/api/clerk", clerkWebhooks)
+app.use("/clerk", clerkWebhooks)
 
 app.get('/', (req, res) => res.send("API is working"));
 
